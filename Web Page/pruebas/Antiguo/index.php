65f667636cb5b069dbdde41f6b57372ad469a0a7
georgeboc <40888335+georgeboc@users.noreply.github.com>
2018-07-20 16:25:29 +0200

    Deleted some proves
    Deleted some proves from the web page folder.

--- Web Page/pruebas/Antiguo/index.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<?php
-header('Location: http://webtecnologia.tuars.com/menu.php#op1');
-?>
-</head>
-</html>
--- Web Page/pruebas/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "
-<h1>Forbidden</h1>
-<br>
-La visualizaci&oacute;n de esta carpeta est&aacute; denegada.
-";
-?>
